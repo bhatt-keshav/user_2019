86728a6c662d160e8cadd29b52873c1a34b3aee2
bhatt-keshav <keshavbhatt@outlook.com>
2019-08-18 20:52:48 +0200

    third commit

--- downloadUser2019.py
+++ downloadUser2019.py
@@ -1,4 +1,7 @@
 import bs4, requests, os, re
+from random import random
+import time
+import win32com.client
 
 website = 'http://www.user2019.fr'
 res = requests.get(website + '/talk_schedule/')
@@ -8,59 +11,94 @@ res.status_code == 200
 user19Soup = bs4.BeautifulSoup(res.text)
 type(user19Soup)
 
-# to test
-names = []
-pdfs = []
-pdfsUrl = []
-
 trFiltered = user19Soup.find_all('tr', class_='filtered')
-trFilteredTest = trFiltered[7:12]
+# temp jugaad: delete
+trFiltered = trFiltered[1:203]
+# def chunks(l, n):
+#     """Yield successive n-sized chunks from l."""
+#     for i in range(0, len(l), n):
+#         yield l[i:i + n]
 
-for tr in trFilteredTest:
+# trFilteredTest = list(chunks(trFiltered, 4))
+# len(trFiltered)
+# len(trFilteredTest)
+# type(trFilteredTest)
+
+# names = []
+
+# for i in range(0, len(trFilteredTest)):
+#     for tr in trFiltered[i]:    
+#         t1, t2, t3 = tr.select("td:nth-of-type(3), td:nth-of-type(4), td:nth-of-type(5)")
+#         t3Alphs = " ".join(re.findall("[a-zA-Z]+", t3.getText()))
+#         tName = t1.getText() + '_' + t2.getText() + '_' + t3Alphs
+#         fileN = tr.a.get('href')
+#         ext = re.search(r'\..*', fileN).group()
+#         print('starting to fetch')
+#         resFile = requests.get(website + fileN)
+#         resFileIter = resFile
+#         resFile.close()
+#         outFile = open(os.getcwd() + '\\' + tName + ext, 'wb')
+#         for chunk in resFileIter.iter_content(1000):
+#             outFile.write(chunk)
+#     outFile.close()
+# print('done')
+names = []
+exts = []
+set(exts)
+usualExts = ['.zip', '.pdf', '.pptx', '.html']
+for tr in trFiltered:
     t1, t2, t3 = tr.select("td:nth-of-type(3), td:nth-of-type(4), td:nth-of-type(5)")
     t3Alphs = " ".join(re.findall("[a-zA-Z]+", t3.getText()))
     tName = t1.getText() + '_' + t2.getText() + '_' + t3Alphs
     fileN = tr.a.get('href')
-    ext = re.search('\..*', fileN).group()
+    names.append(fileN)    
+    ext = re.search(r'\..*', fileN).group()
+    exts.append(ext)
+    # if exts in ... then go ahead, otherwise skip
+    print('starting to fetch')
     resFile = requests.get(website + fileN)
+    resFileIter = resFile
+    resFile.close()
     outFile = open(os.getcwd() + '\\' + tName + ext, 'wb')
-    for chunk in resFile.iter_content(100000):
+    for chunk in resFileIter.iter_content(1000):
         outFile.write(chunk)
     outFile.close()
+    print('sleeping')
+    time.sleep(random() * 60)
 print('done')
   
-# pilot example to put in for loop
-fileN = trFiltered[2].a.get('href')
-ext = re.search('\..*', fileN).group()
-resFile = requests.get(website + fileN)
-outFile = open(os.getcwd() + '\\' + 'test' + ext, 'wb')
-for chunk in resFile.iter_content(100000):
-    outFile.write(chunk)
-outFile.close()
 
-# check why hannah is not working
-tr = trFiltered[9]
+# standard case
+tr1 = trFiltered[3]
+tr1Ext =  re.search(r'\..*', tr1.a.get('href')).group()
+tr1Ext in usualExts
+# check why nr 4 is not working
+tr = trFiltered[5]
 t1, t2, t3 = tr.select("td:nth-of-type(3), td:nth-of-type(4), td:nth-of-type(5)")
 t3Alphs = " ".join(re.findall("[a-zA-Z]+", t3.getText()))
 tName = t1.getText() + '_' + t2.getText() + '_' + t3Alphs
 fileN = tr.a.get('href')
-ext = re.search('\..*', fileN).group()
-resFile = requests.get(website + fileN)
-outFile = open(os.getcwd() + '\\' + tName + ext, 'wb')
-for chunk in resFile.iter_content(100000):
-    outFile.write(chunk)
-outFile.close()
+ext = re.search(r'\..*', fileN).group()
+if ext in usualExts:
+    resFile = requests.get(website + fileN)
+    outFile = open(os.getcwd() + '\\' + tName + ext, 'wb')
+    resFileIter = resFile
+    resFile.close()
+    for chunk in resFileIter.iter_content(100000):
+        outFile.write(chunk)
+    outFile.close()
+else:
+    
+   
+urlPath = os.getcwd() + '\\' + tName + '.url'
+fileN #target
+# target = 'http://www.google.com/'
+ws = win32com.client.Dispatch("wscript.shell")
+shortcut = ws.CreateShortcut(urlPath)
+shortcut.TargetPath = fileN
+shortcut.Save()
+
 
 re.search('^\w+.*',tName)
 " ".join(re.findall("[a-zA-Z]+", tName))
 
-
-# TRY
-# need 4, 5, 8
-a1, a2 = trFiltered[1].select("td:nth-of-type(4), td:nth-of-type(5)")
-type(a1.text) #str, but cannot concatenate, still in tag class
-type(a1.getText()) #str good
-a3 = a1.getText() + '_' + a2.getText()
-a = []
-a.append(a3)
-a.append(a1)
